f03f1d4473e1177905af10d39dcae8465f72dc02
Arpit <13appushridhar@gmail.com>
2024-10-16 15:50:42 +0530

    final code of validation

--- src/main/java/com/cpt/payments/controller/AdditionController.java
+++ src/main/java/com/cpt/payments/controller/AdditionController.java
@@ -1,12 +1,19 @@
 package com.cpt.payments.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cpt.payments.service.interfaces.ReconService;
+
 @RestController
 public class AdditionController {
+	
+	private ReconService reconService;
+	
+	public AdditionController(ReconService reconService) {
+		this.reconService = reconService;
+	}
 	
     @PostMapping("/add")
     public int add(@RequestParam int num1, @RequestParam int num2) {
@@ -16,5 +23,14 @@ public class AdditionController {
         System.out.println("sumResult:" + sumResult);
 
         return sumResult;
+    }
+    
+    @PostMapping("/testRecon")
+    public String testReconLogic() {
+    	
+    	reconService.reconcilePayments();
+    	
+    	return "Recon triggered.";
+    	
     }
 }
